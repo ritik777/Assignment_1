a3cc164a9854d3e08ab30c7944ec25db164f3b3c
Ritik Gupta <ritik.gupta95@gmail.com>
2020-01-24 11:13:05 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        private static void PrintPattern(int n)
+        {
+            int i, j, q;
+            for (i = n; i >= 1; i--)
+            {
+
+                for (j = i; j >= 1; j--)
+                {
+                    Console.Write(j);
+                }
+
+                Console.WriteLine(" ");
+                n--;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            PrintPattern(5);
+        }
+    }
+}
